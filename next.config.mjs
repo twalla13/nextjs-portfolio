48c9426adfd5d5ea69170b2c511e35c5c738c5e3
Toni Wallace <twalla13@asu.edu>
2024-05-15 16:34:39 -0400

    original buildspec, update package json for export (out) and next config got static website

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+// next.config.js
+module.exports = {
+  output: 'standalone',
+  images: {
+    domains: ['tonidocs.com'], // Customize to your needs
+  }
+};
 
 export default nextConfig;
