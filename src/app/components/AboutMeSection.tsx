ae2d68b07c6d184c1d99d77fe98d045f1fd14409
Toni Wallace <twalla13@asu.edu>
2024-08-01 14:32:30 -0400

    custom text for highlight content

--- src/app/components/AboutMeSection.tsx
+++ src/app/components/AboutMeSection.tsx
@@ -18,15 +18,15 @@ const AboutMeSection = () => (
               border: '5px solid #EECD56',
               boxShadow: '5px 5px 20px 0px rgba(255, 230, 140, 0.13)'
             }}>
-              <h3 className="text-xl font-bold">Card 1</h3>
+              <h3 className="custom-text ">Card 1</h3>
               <p>This is the first card.</p>
             </div>
           </div>
           <div className="relative flex-shrink-0 w-[377.67px] h-[452px] " style={{ transform: 'rotate(4deg)' }}>
             <div className="p-4 h-full" style={{
-              backgroundColor: '#FFE68C',
+              backgroundColor: '#9DDCFF',
               borderRadius: '8px',
-              border: '5px solid #EECD56',
+              border: '5px solid #5AB5E8',
               boxShadow: '5px 5px 20px 0px rgba(255, 230, 140, 0.13)'
             }}>
               <h3 className="text-xl font-bold">Card 2</h3>
@@ -35,9 +35,9 @@ const AboutMeSection = () => (
           </div>
           <div className="relative flex-shrink-0 w-[377.67px] h-[452px]" style={{ transform: 'rotate(-4deg)' }}>
             <div className="p-4 h-full" style={{
-              backgroundColor: '#FFE68C',
+              backgroundColor: '#FFC9F0',
               borderRadius: '8px',
-              border: '5px solid #EECD56',
+              border: '5px solid #F384D4',
               boxShadow: '5px 5px 20px 0px rgba(255, 230, 140, 0.13)'
             }}>
               <h3 className="text-xl font-bold">Card 3</h3>
