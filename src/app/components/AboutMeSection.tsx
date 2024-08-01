56e5379c954a93104346999e710beaa494a21c35
Toni Wallace <twalla13@asu.edu>
2024-08-01 15:55:43 -0400

    Add about me section and custom text

--- src/app/components/AboutMeSection.tsx
+++ src/app/components/AboutMeSection.tsx
@@ -12,36 +12,36 @@ const AboutMeSection = () => (
         </div>
          <div className="grid flex-row grid-cols-1 md:grid-cols-3 gap-16">
           <div className="relative flex-shrink-0 w-[377.67px] h-[452px]" style={{ transform: 'rotate(-4deg)' }}>
-            <div className="p-4 h-full" style={{
+            <div className="p-4 h-full flex flex-col items-center justify-center" style={{
               backgroundColor: '#FFE68C',
               borderRadius: '8px',
               border: '5px solid #EECD56',
               boxShadow: '5px 5px 20px 0px rgba(255, 230, 140, 0.13)'
             }}>
-              <h3 className="custom-text ">Card 1</h3>
-              <p>This is the first card.</p>
+              <h3 className="custom-text absolute top-[80px] right-[-40px]" style={{ backgroundColor: '#9DDCFF', transform: 'rotate(4deg)' }}>Cloud Services</h3>
+              <p className="font-body text-2xl"style={{transform: 'rotate(4deg)'}}>Utilize AWS and Azure for scalable, efficient cloud solutions.</p>
             </div>
           </div>
           <div className="relative flex-shrink-0 w-[377.67px] h-[452px] " style={{ transform: 'rotate(4deg)' }}>
-            <div className="p-4 h-full" style={{
+            <div className="p-4 h-full flex flex-col items-center justify-center" style={{
               backgroundColor: '#9DDCFF',
               borderRadius: '8px',
               border: '5px solid #5AB5E8',
               boxShadow: '5px 5px 20px 0px rgba(255, 230, 140, 0.13)'
             }}>
-              <h3 className="text-xl font-bold">Card 2</h3>
-              <p>This is the second card.</p>
+              <h3 className="custom-text absolute top-[-20px] " style={{ transform: 'rotate(-4deg)' }}>Object Orientated Programming</h3>
+              <p className="font-body text-2xl" style={{transform: 'rotate(-4deg)' }}>Develop scalable, maintainable software using Java, C++, and Python.</p>
             </div>
           </div>
           <div className="relative flex-shrink-0 w-[377.67px] h-[452px]" style={{ transform: 'rotate(-4deg)' }}>
-            <div className="p-4 h-full" style={{
+            <div className="p-4 h-full flex flex-col items-center justify-center" style={{
               backgroundColor: '#FFC9F0',
               borderRadius: '8px',
               border: '5px solid #F384D4',
               boxShadow: '5px 5px 20px 0px rgba(255, 230, 140, 0.13)'
             }}>
-              <h3 className="text-xl font-bold">Card 3</h3>
-              <p>This is the third card.</p>
+              <h3 className="custom-text absolute bottom-[320px] right-[-40px]" style={{ backgroundColor: '#FFE68C', transform: 'rotate(4deg)' }}>Web Development</h3>
+              <p className="font-body text-2xl " style={{transform: 'rotate(4deg)' }}>Create responsive, dynamic web applications with React, NextJS, Node.js, and TypeScript.</p>
             </div>
           </div>
         </div>
