296d358351738efa7bb6ab6ae37291ec924e79d6
Toni Wallace <twalla13@asu.edu>
2024-08-02 16:35:28 -0400

    actually fixed breakpoints, fixed custom text highlight, fixed breakpoints on homepage

--- src/app/components/AboutMeSection.tsx
+++ src/app/components/AboutMeSection.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 const AboutMeSection = () => (
- <section id= "about" className="flex items-center justify-center p-8">
+ <section id= "about" className="flex items-center justify-center lg:p-8">
       <div className="grid grid-cols-1 gap-4">
         <div className="relative flex justify-start mb-2">
           <img
@@ -10,41 +10,41 @@ const AboutMeSection = () => (
             className="w-164 h-153"
           />
         </div>
-         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-12 lg:gap-16 xl:gap-20">
+         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-20 lg:gap-10">
         {/* Card 1 */}
-          <div  className="relative w-full md:w-[280px] lg:w-[300px] xl:w-[350px] 2xl:w-[377.67px] h-[380px] md:h-[400px] lg:h-[420px] xl:h-[440px] 2xl:h-[452px]" style={{ transform: 'rotate(-4deg)' }}>
+          <div  className="relative w-full h-[240px] md:w-[240px] lg:w-full  md:h-[360px] lg:h-500" style={{ transform: 'rotate(-4deg)' }}>
             <div className="p-4 h-full flex flex-col items-center justify-center" style={{
               backgroundColor: '#FFE68C',
               borderRadius: '8px',
               border: '5px solid #EECD56',
               boxShadow: '5px 5px 20px 0px rgba(255, 230, 140, 0.13)'
             }}>
-              <h3 className="custom-text absolute top-[80px] right-[-40px]" style={{ backgroundColor: '#9DDCFF', transform: 'rotate(4deg)' }}>Cloud Services</h3>
-              <p className="font-body text-2xl text-center" style={{transform: 'rotate(4deg)'}}>Utilize AWS and Azure for scalable, efficient cloud solutions.</p>
+              <h3 className="custom-text absolute top-[50px] right-[-20px] md:top-[70px] md:right-[-40px]" style={{ backgroundColor: '#9DDCFF', transform: 'rotate(4deg)' }}>Cloud Services</h3>
+              <p className="font-body  text-xl lg:text-2xl text-center" style={{transform: 'rotate(4deg)'}}>Utilize AWS and Azure for scalable, efficient cloud solutions.</p>
             </div>
           </div>
           {/* Card 2 */}
-          <div  className="relative w-full md:w-[280px] lg:w-[300px] xl:w-[350px] 2xl:w-[377.67px] h-[380px] md:h-[400px] lg:h-[420px] xl:h-[440px] 2xl:h-[452px]" style={{ transform: 'rotate(4deg)' }}>
+          <div  className="relative w-full h-[240px] md:w-[240px] lg:w-full md:h-[360px] lg:h-500" style={{ transform: 'rotate(4deg)' }}>
             <div className="p-4 h-full flex flex-col items-center justify-center" style={{
               backgroundColor: '#9DDCFF',
               borderRadius: '8px',
               border: '5px solid #5AB5E8',
               boxShadow: '5px 5px 20px 0px rgba(255, 230, 140, 0.13)'
             }}>
-              <h3 className="custom-text absolute top-[-20px] " style={{ transform: 'rotate(-4deg)' }}>Object Orientated Programming</h3>
-              <p className="font-body text-2xl" style={{transform: 'rotate(-4deg)' }}>Develop scalable, maintainable software using Java, C++, and Python.</p>
+              <h3 className="custom-text absolute top-[-20px]" style={{ transform: 'rotate(-4deg)' }}>Object Orientated Programming</h3>
+              <p className="font-body text-xl lg:text-2xl  text-center" style={{transform: 'rotate(-4deg)' }}>Develop scalable, maintainable software using Java, C++, and Python.</p>
             </div>
           </div>
           {/* Card 3 */}
-          <div  className="relative w-full md:w-[280px] lg:w-[300px] xl:w-[350px] 2xl:w-[377.67px] h-[380px] md:h-[400px] lg:h-[420px] xl:h-[440px] 2xl:h-[452px]"style={{ transform: 'rotate(-4deg)' }}>
+          <div  className="relative w-full h-[240px] md:w-[240px] lg:w-full md:h-[360px] lg:h-full" style={{ transform: 'rotate(-4deg)' }}>
             <div className="p-4 h-full flex flex-col items-center justify-center" style={{
               backgroundColor: '#FFC9F0',
               borderRadius: '8px',
               border: '5px solid #F384D4',
               boxShadow: '5px 5px 20px 0px rgba(255, 230, 140, 0.13)'
             }}>
-              <h3 className="custom-text absolute bottom-[320px] right-[-40px]" style={{ backgroundColor: '#FFE68C', transform: 'rotate(4deg)' }}>Web Development</h3>
-              <p className="font-body text-2xl text-center" style={{transform: 'rotate(4deg)' }}>Create responsive, dynamic web applications with React, NextJS, Node.js, and TypeScript.</p>
+              <h3 className="custom-text absolute bottom-[170px] right-[-20px] md:bottom-[320px] md:right-[-40px]" style={{ backgroundColor: '#FFE68C', transform: 'rotate(4deg)' }}>Web Development</h3>
+              <p className="font-body text-xl lg:text-2xl text-center" style={{transform: 'rotate(4deg)' }}>Create responsive, dynamic web applications with React, NextJS, Node.js, and TypeScript.</p>
             </div>
           </div>
         </div>
--- src/app/components/HomeSection.tsx
+++ src/app/components/HomeSection.tsx
@@ -2,17 +2,17 @@
 import React from 'react';
 
 const HomeSection = () => (
-  <section id="homepage" className="mx-auto md:mx-48 flex flex-col lg:flex-row items-center justify-center">
+  <section id="homepage" className="mx-auto flex lg:mx-48 flex-col lg:flex-row items-center justify-center">
       <div>
         <img
           src="/imgs/homepagegirl.png"
           alt="Homepage Girl"
-          className="w-90 h-105 rounded-full"
+          className="w-40 h-40 md:w-60 md:h-60 lg:w-64 lg:h-80 xl:w-80 xl:h-80 rounded-full"
         />
       </div>
-      <div className="text-center mt-6 lg:text-left lg:p-2">
-        <h1 className="font-display text-3xl lg:text-6xl font-bold mb-4">Toniann Wallace: Crafting Digital Solutions</h1>
-        <p className="mb-4 font-display lg:text-2xl">With degrees in Math and Software Engineering, I am a full stack engineer skilled in problem-solving and multiple coding languages. Outgoing and ready to innovate.</p>
+      <div className="text-center mt-6 p-4 lg:text-left lg:p-2">
+        <h1 className="font-display text-2xl md:text-3xl lg:text-6xl font-bold mb-4">Toniann Wallace: Crafting Digital Solutions</h1>
+        <p className="mb-4 font-display lg:text-2xl sm:mx-4">With degrees in Math and Software Engineering, I am a full stack engineer skilled in problem-solving and multiple coding languages. Outgoing and ready to innovate.</p>
         <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">Hire Me</button>
       </div>
     </section>
